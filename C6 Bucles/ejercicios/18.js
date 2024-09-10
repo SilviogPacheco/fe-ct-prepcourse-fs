b72facad75059b3595205e89adae4b38d82f4bad
SilviogPacheco <megaboy93mile@gmail.com>
2024-09-09 21:55:38 -0300

    Ejercicios C6 resueltos

--- C6 Bucles/ejercicios/18.js
+++ C6 Bucles/ejercicios/18.js
@@ -2,6 +2,14 @@ function productoEntreNúmeros(a, b) {
   // Dados dos argumentos "a" y "b", devuelve el producto de todos
   // los números entre a y b (inclusive).
   // Tu código:
+  let producto = 1;
+  if (a <= 0 && b >= 0) {
+    return 0;
+  }
+  for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+    producto *= i;
+  }
+  return producto;
 }
 
 module.exports = productoEntreNúmeros;
